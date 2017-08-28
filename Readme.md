efbcb9ed2a2857eba94e327dede74b22a1ef404f
Rahul <shukla2009@gmail.com>
2017-08-28 20:17:39 +0530

    initial commit

---
+++ Readme.md
@@ -0,0 +1,27 @@
+ip-resolver rest service
+===
+
+This repository provides a rest service to resolve location from ip
+
+prerequsite
+---
+Node js and npm should be installed
+
+steps to run:
+---
+
+```bash
+$ git clone  https://github.com/shukla2009/assignment-ip-resolver.git
+$ cd ip-resolver
+$ npm install
+$ npm start
+```
+
+build process
+---
+```bash
+$ git clone https://github.com/shukla2009/assignment-ip-resolver.git
+$ cd ip-resolver
+$ npm install
+$ zip/tar entire app make sure to create with build number
+```
---
+++ app.js
@@ -0,0 +1,47 @@
+'use strict';
+var config = require('./config');
+var logger = require('./logger')(module);
+var util = require('util');
+var https = require('https');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('This is ip resolver service');
+});
+
+app.get('/ip/:ip', function (req, res) {
+    logger.info('resolving : ' + req.params.ip);
+    if ('127.0.0.1' === req.params.ip || '1'===req.params.ip) {
+        return res.send({location: 'localhost'});
+    }
+    let options = {
+        host: 'freegeoip.net',
+        port: 443,
+        path: `/json/${req.params.ip}`,
+        method: 'GET'
+    };
+
+    let request = https.request(options, function (result) {
+        let message = '';
+        result.on('data', function (chunk) {
+            message += chunk;
+        });
+        result.on('end', function () {
+            if (result.statusCode !== 200) {
+                return res.send(JSON.parse(message));
+            }
+            res.status(404).send('Not Found');
+        });
+    });
+    request.end();
+});
+app.all('/*', function (req, res) {
+    res.status(404).send('Not Found');
+});
+
+app.set('port', config.port);
+
+var server = app.listen(app.get('port'), function () {
+    logger.info(util.format('Express server listening on port %s', server.address().port));
+});
